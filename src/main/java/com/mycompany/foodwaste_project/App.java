9202be50248632234519512363cef7608d55cc9a
Kasper Svane <svane.kasper20@gmail.com>
2019-11-28 17:23:19 +0100

    Starten på spillet er blevet lavet

--- src/main/java/com/mycompany/foodwaste_project/App.java
+++ src/main/java/com/mycompany/foodwaste_project/App.java
@@ -9,6 +9,7 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 import javafx.application.Platform;
+import javafx.stage.StageStyle;
 
 /**
  * JavaFX App
@@ -24,7 +25,7 @@ public class App extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         
-        scene = new Scene(loadFXML("Outside"), 640, 480);
+        scene = new Scene(loadFXML("Startmenu"), 640, 480);
         stage.setScene(scene);
         stage.show();
     }
