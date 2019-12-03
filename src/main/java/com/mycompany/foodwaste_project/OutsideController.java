7a0f9d97a0fb5510c458ed95207df8f404adcf4d
Jakup Malik Davut Karagoz <cavras2009@hotmail.com>
2019-12-03 15:16:52 +0100

    "Added smartphone"

---
+++ src/main/java/com/mycompany/foodwaste_project/OutsideController.java
@@ -0,0 +1,92 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.foodwaste_project;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+
+import javafx.scene.control.Button;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+/**
+ * FXML Controller class
+ *
+ * @author Cavra
+ */
+public class OutsideController implements Initializable {
+
+
+    @FXML
+    private Button apartment;
+    @FXML
+    private Button mcDonalds;
+    @FXML
+    private Button fakta;
+    @FXML
+    private Button naturmarket;
+    @FXML
+    private Button smartphoneButton;
+    @FXML
+    private Button statsButton;
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+    @FXML
+    private void goApartment(ActionEvent event) throws IOException {
+        App.setRoot("Apartment");
+    }
+
+    @FXML
+    private void goMcDonalds(ActionEvent event) throws IOException {
+        App.setRoot("mcDonalds");
+    }
+
+    @FXML
+    private void goFakta(ActionEvent event) throws IOException {
+        App.setRoot("Fakta");
+    }
+
+    @FXML
+    private void goNaturmarket(ActionEvent event) throws IOException {
+        App.setRoot("Naturmarket");
+    }
+
+    @FXML
+    private void openSmartphone(ActionEvent event) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("Smartphone.fxml"));
+        Parent parent = (Parent) fxmlLoader.load();
+        Stage stage = new Stage();
+        stage.initModality(Modality.WINDOW_MODAL);
+        //stage.initStyle(StageStyle.UNDECORATED);
+        stage.setScene(new Scene(parent, 618, 661));
+        stage.show();
+    }
+
+    @FXML
+    private void viewStats(ActionEvent event) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("Stats.fxml"));
+        Parent parent = (Parent) fxmlLoader.load();
+        Stage stage = new Stage();
+        stage.initModality(Modality.WINDOW_MODAL);
+        stage.initStyle(StageStyle.UNDECORATED);
+        stage.setScene(new Scene(parent, 600, 241));
+        stage.show();
+    }
+
+}
--- src/main/java/com/mycompany/foodwaste_project/SmartphoneController.java
+++ src/main/java/com/mycompany/foodwaste_project/SmartphoneController.java
@@ -1,29 +1,20 @@
-package com.mycompany.foodwaste_project;
-
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+package com.mycompany.foodwaste_project;
 
 import java.net.URL;
 import java.util.ResourceBundle;
-import javafx.event.ActionEvent;
-import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Button;
-import javafx.scene.layout.AnchorPane;
-import javafx.stage.Stage;
 
 /**
  * FXML Controller class
  *
  * @author Cavra
  */
-public class StatsController implements Initializable {
-
-    @FXML
-    private Button closeButton;
+public class SmartphoneController implements Initializable {
 
     /**
      * Initializes the controller class.
@@ -31,13 +22,6 @@ public class StatsController implements Initializable {
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
-    }
-
-    @FXML
-    private void closeButtonAction(ActionEvent event) {
-        Stage stage = (Stage) closeButton.getScene().getWindow();
-        stage.close();
-    }
-    
+    }    
     
 }
